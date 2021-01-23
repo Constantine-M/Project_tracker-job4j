16f1548e477f2e429ab9bc1fc390ab573a90147f
Constantine-M <mez365@mail.ru>
2021-01-23 21:40:08 +0300

    1.4. Вызов метода с возвращаемым типом. [#347587]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+    /**Создали метод, который возвращает переменную типа String*/
+    public String engToRus(String eng) {
+       String word = "Неизвестное слово. " + eng;
+       return word;
+    }
+
+    /**Теперь объект dictionary класса DummyDic обращается к методу engToRus.
+     * Метод возвращает словосочетание "Неизвестное слово" и то что мы введем.
+     * А потом нам нужно это вывести в консоль с помощью sout.
+     */
+    public static void main(String[] args) {
+        DummyDic dictionary = new DummyDic();
+        String unknown = dictionary.engToRus("Sireneven'kii refrezherator");
+        System.out.println(unknown);
+    }
+}
