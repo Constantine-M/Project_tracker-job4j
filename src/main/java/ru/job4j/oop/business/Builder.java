f4c8999cebd95f9fe70932151b37eb84f60b17b8
Constantine-M <mez365@mail.ru>
2021-01-31 15:10:41 +0300

    3. Реализация профессий в коде [#347618]

--- src/main/java/ru/job4j/oop/business/Builder.java
+++ src/main/java/ru/job4j/oop/business/Builder.java
@@ -5,7 +5,8 @@ package ru.job4j.oop.business;
  * Чтобы наследовать один класс от другого,
  * нужно после имени класса написать ключевое слово extends
  * и далее указать, какой класс нужно наследовать.
- */
+
 public class Builder extends Engineer {
 
 }
+*/
--- src/main/java/ru/job4j/oop/business/Programmer.java
+++ src/main/java/ru/job4j/oop/business/Programmer.java
@@ -4,7 +4,7 @@ package ru.job4j.oop.business;
  * Чтобы наследовать один класс от другого,
  * нужно после имени класса написать ключевое слово extends
  * и далее указать, какой класс нужно наследовать.
- */
+
 public class Programmer extends Engineer {
 
-}
+}*/
