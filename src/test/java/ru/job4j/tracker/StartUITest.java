edad59c3f60b5482356b40749be870f7914a141c
Constantine-M <mez365@mail.ru>
2021-03-14 14:06:18 +0300

    9. Написать тесты на StartUI. [#347560]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,79 +1,89 @@
-/**package ru.job4j.tracker;
+package ru.job4j.tracker;
 
 import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class StartUITest {
-
+    /**
+     * Для понимания, как написан тест.
+     * "0", "Item name", "1" - эта строка есть алгоритм действий.
+     * То есть {0, 1} - это индексы массива с действиями, который мы написали ниже.
+     * UserAction[] actions = {2 действия} - то есть мы действия задали сами.
+     * Нам ведь нужно только создать и выйти.
+     * "Item name" - это то, что будет создано после действия "0" (создание).
+     * Input in = new StubInput(new String[] {то что пользователь введет} - здесь мы
+     * эмулируем ввод пользователя. То есть именно в таком порядке он будет выбирать пункт (создание),
+     * называть элемент и выбирать 1 для выхода.
+     */
     @Test
     public void whenCreateItem() {
-        String[] answers = {"Consta"};
-        Input input = new StubInput(answers);
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+                );
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Consta");
-        assertThat(created.getName(), is(expected.getName()));
-
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
     /**
-     * Создаем предполагаемый ответ пользователя - String[] answers = {"FX PC"};
-     * Создаем объект класса StubInput с аргументом answers, эмулируя ввод инфы пользователя.
-     * Создаем заявку - StartUI.createItem(input, tracker);
-     * Данная заявка в массиве имеет индекс = 0. Это всё, что должен найти наш проверяемый метод.
-     * Согласно методу, мы должны найти все заявки. А так как создали одну, то
-     * expected = <создать эту самую заявку> - = new Item("FX PC");
-     * Сравниваем created и expected.
+     * 1. Эмулируем ввод, создав последовательность ввода данных в виде массива
+     * {создать - имя - заменить - выйти}.
+     * 2. Создаем объект класса Tracker.
+     * 3. Создаем массив действий, которые будем/хотим совершить.
+     * 4. Создаем объект класса StartUI и вызываем у него метод init с аргументами
+     * (in, tracker, actions), которые описывают ввод, методы над заявками и действиям (ввиду использования интерфейса)
+     * 5. Сравниваем имя найденной заявки с ожидаемой.
      */
-/**
-    @Test
-    public void whenFindAllItems() {
-        String[] answers = {"FX PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("FX PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    /**
-     * 1. Создаем объект tracker.
-     * 2. Создаем объект item.
-     * 3. Добавляем item в tracker. После этой операции у нас есть id.
-     * 4. Достаем item.id и создаем массив с ответами пользователя.
-     * 5. Вызываем тестируемый метод replaceItem. Это действие изменит состояние объекта tracker.
-     * 6. Ищем по item.id замененный item в объекте tracker.
-     * 7. Сравниваем имя найденной заявки с ожидаемой.
-     */
-    /**
     @Test
     public void whenReplaceItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1", "1", "Replaced Item", "2"}
+        );
         Tracker tracker = new Tracker();
-        Item item = new Item("new Item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "replaced Item"
+        UserAction[] actions = {
+                new CreateAction(),
+                new ReplaceAction(),
+                new ExitAction()
         };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced Item"));
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Replaced Item"));
     }
-/**
+
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("new Item");
-        tracker.add(item);
-        String[] answer = {
-                String.valueOf(item.getId())
+        Item item = tracker.add(new Item("Deleted Item"));
+        Input in = new StubInput(new String[]
+                {"0", "1", "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
         };
-        StartUI.deleteItem(new StubInput(answer), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted, is(nullValue()));
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
-}*/
+
+    /**
+     * Тест максимально поход на создание заявки.
+     */
+    @Test
+    public void whenFindAllItems() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("FX PC find all"));
+        Input in = new StubInput(new String[]
+                {"0", "1"}
+                );
+        UserAction[] actions = {
+                new FindAllAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("FX PC find all"));
+    }
+}
