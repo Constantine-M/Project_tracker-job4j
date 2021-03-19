43ccee6980db6fd5e666f6f54f04e71aa027f8db
Constantine-M <mez365@mail.ru>
2021-03-19 19:08:18 +0300

    Тестовый класс

---
+++ src/main/java/ru/job4j/test/Test.java
@@ -0,0 +1,11 @@
+package ru.job4j.test;
+
+public class Test {
+    public void print(Object obj){
+        StringBuilder str = new StringBuilder();
+        str.append(obj.toString());
+    }
+    public static void main(String[] args) {
+
+    }
+}
