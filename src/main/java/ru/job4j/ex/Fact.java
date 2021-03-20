0258050ab81f3e1309827c155c3eb95ff9767bac
Constantine-M <mez365@mail.ru>
2021-03-20 20:34:52 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#347567]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println("Factorial value: " + calc(-5));
+    }
+
+    public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N must be positive");
+        }
+        int rsl = 1;
+        for (int index = rsl; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
