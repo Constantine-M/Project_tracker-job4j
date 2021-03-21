9d835117b6842c8513da0f423a93779fa4ea9f8a
Constantine-M <mez365@mail.ru>
2021-03-21 13:42:51 +0300

    0.4. Пользовательские исключения. [#347569]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,40 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    /**
+     * Unhandled exception - необработанное исключение.
+     * Если мы используем в коде классы не имеющие в иерархии наследования
+     * java.langRuntimeException, то их нужно обрабатывать.
+     * Первый способ - это объявления исключений в сигнатуре метода
+     * с помощью throws.
+     * В этом случае мы передаем ответственность за исключение коду, который будет использовать наш метод.
+     */
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found.");
+        }
+        return rsl;
+    }
+
+    /**
+     * Второй вариант - это использование конструкции try-catch.
+     * Блок try содержит операторы, которые мы хотим выполнить.
+     * Блок catch содержит объявление исключение.
+     * Если операторы в блоке try вызовут исключение UserInputException,
+     * то программа перейдет к выполнению блока catch.
+     */
+    public static void main(String[] args) {
+        String[] list = {"Consta", "Petr", "Stas", "Zack"};
+        try {
+            System.out.println(indexOf(list, "Mezenin"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/UserInputException.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+/**
+ * Класс java.lang.Exception имеет конструктор по умолчанию.
+ * Petr Arsentev рекомендует всегда добавлять конструктор с текстом.
+ * При использовании вашего исключения нужно будет добавить детальную
+ * информацию о возникновении исключения, а не просто получить пустой вывод.
+ */
+public class UserInputException extends Exception {
+    public UserInputException(String message) {
+        super(message);
+    }
+}
