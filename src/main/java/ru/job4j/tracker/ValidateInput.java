9ddda956541fd4a770ae36ad36e15d109b007c30
Constantine-M <mez365@mail.ru>
2021-03-26 22:16:30 +0300

    2.1. Тест на ValidateInput [#347573]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -37,7 +37,7 @@ public class ValidateInput implements Input {
                 value = in.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again.");
+                out.println("Please enter validate data again.");
             }
             /**
              * С консоли вводим значение, а затем его приводим к числу с помощью метода parseInt().
---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        /**
+         * Эмулируем ввод. Сначала неверный вариант, потом верный.
+         */
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        /**Создаем объект класса ValidateInput, чтобы получить доступ к методу, который проверяем собственно.*/
+        ValidateInput input = new ValidateInput(out, in);
+        /**
+         * int selected - это как раз тот самый результат работы метода askInt в классе ValidateInput.
+         * То есть ввод данных будет сопровожден стройкой "Enter menu:". Этот текст не должен тебя путать.
+         * Он может быть любым.
+         * Мы проверяем метод askInt, в результате работы которого метод из 2 вариантов ("one", "1")
+         * должен выбрать правильный - 1.
+         */
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    /**Не понимаю, чем он отличается от первого теста.*/
+    @Test
+    public void whenCorrectInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenCorrectInputManyTimes() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        int[] expected = {0, 1};
+        assertThat(selected, is(expected));
+    }
+
+    @Test
+    public void whenNegativeInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
+}
