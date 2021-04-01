6f367caf86f87583a53e22958485dd5263960aaf
Constantine-M <mez365@mail.ru>
2021-04-01 21:08:51 +0300

    2. Singleton. [#347601] - правки #2

--- src/test/java/ru/job4j/tracker/SingleTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SingleTrackerTest.java
@@ -11,12 +11,14 @@ public class SingleTrackerTest {
     /**
      * Насколько я понял - метод возвращает объект.
      * При этом он возвращает не новый объект, а один и тот же.
+     * Ссылки на объект должны быть одинаковыми.
+     * Это можно проверить с помощью assertSame.
      */
     @Test
     public void whenSingletonIsSetUp() {
         SingleTracker first = SingleTracker.getInstance();
         SingleTracker second = SingleTracker.getInstance();
-        Assert.assertEquals(first, second);
+        Assert.assertSame(first, second);
 
     }
 }
