e82bd07e6b96b9691065a7fe61bcb301afd51dcc
Constantine-M <mez365@mail.ru>
2021-05-07 12:29:47 +0300

    0. Коллекции, ArrayList, List, Обобщения [#347532]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 
 /**
  * Данный класс показывает на примере, что из себя представляют коллекции и как с ними можно работать.
+ * При добавлении записи в коллекцию не обязательно указывать индекс.
+ * Индекс в коллекциях применяется/используется реже, чем в массивах.
+ * Далее мы познакомимся с Мар (тоже коллекция). В некоторых случаях она больше подходит.
  */
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
-        names.add(0, "Petr");
-        names.add(1, "Ivan");
-        names.add(2, "Stepan");
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
         for (String list : names) {
             System.out.println(list);
         }
