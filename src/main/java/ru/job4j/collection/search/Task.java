0731aaa149c4eb9363bdd27f2c75003ad7c0e4df
Constantine-M <mez365@mail.ru>
2021-05-08 12:44:52 +0300

    2. Очередь с приоритетом на LinkedList [#347527]

---
+++ src/main/java/ru/job4j/collection/search/Task.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection.search;
+
+
+/**
+ * Данный класс связан с задачей "Очередь с приоритетом на LinkedList".
+ * Класс описывает задачу, у который есть описание и уровень важности.
+ */
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
