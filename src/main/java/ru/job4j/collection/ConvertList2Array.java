0d3d798ab89575ea4f1cafc53447abd49b0f08f4
Constantine-M <mez365@mail.ru>
2021-05-09 16:21:44 +0300

    3. Конвертация ArrayList в двухмерный массив [#347529]

---
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -0,0 +1,45 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Данный класс соответствует уроку "Конвертация ArrayList в двухмерный массив".
+ */
+public class ConvertList2Array {
+    /**
+     * С помощью метода Math.ceil мы можем округлять в большую сторону.
+     * Результат метода возвращает тип double, а нам нужен int, поэтому произвели явное приведение.
+     * @param list список.
+     * @param cells ячейка в двухмерном массиве (кол-во).
+     * @return
+     */
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        System.out.println(groups);
+        int [][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            if (cell > 2) {
+                row++;
+                cell = 0;
+            }
+            array[row][cell++] = num;
+        }
+        return array;
+    }
+
+    /**
+     * В первой строке создали из набора элементов (1, 2, 3...) объект List с помощью метода List.of.
+     */
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
+        int[][] rsl = toArray(list, 3);
+        for (int[] row : rsl) {
+            for (int cell : row) {
+                System.out.print(cell + "");
+            }
+            System.out.println();
+        }
+    }
+}
