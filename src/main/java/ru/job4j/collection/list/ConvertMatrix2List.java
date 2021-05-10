372141722f15b15dccd5c5166ca6335ef6953184
Constantine-M <mez365@mail.ru>
2021-05-10 16:00:22 +0300

    4. Конвертация двумерного массива в ArrayList [#347528]

---
+++ src/main/java/ru/job4j/collection/list/ConvertMatrix2List.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertMatrix2List {
+    /**
+     * Метод, в который мы передаем двумерный массив, возвращает список.
+     * Используется вложенный цикл for-each.
+     * В первом цикле мы проходимся по строкам, поэтому указано int[] row.
+     * А во втором цикле мы проходимся по ячейкам одномерного массива int[] row.
+     * В теле  цикла добавляем ячейку в список.
+     * @param array двумерный массив.
+     * @return список с данными, которые записали из ячеек массива.
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] row : array) {
+            for (int cell : row) {
+                list.add(cell);
+            }
+        }
+        return list;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection.list;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import java.util.List;
+
+public class ConvertMatrix2ListTest {
+
+    @Test
+    public void whenCells3ThenListOf9Elements() {
+        ConvertMatrix2List array = new ConvertMatrix2List();
+        List<Integer> result = array.toList(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenCells2ThenListOf4Elements() {
+        ConvertMatrix2List array = new ConvertMatrix2List();
+        List<Integer> result = array.toList(new int[][]{
+                {1, 2},
+                {3, 4},
+        });
+        List<Integer> expected = List.of(1, 2, 3, 4);
+        assertThat(result, is(expected));
+    }
+}
