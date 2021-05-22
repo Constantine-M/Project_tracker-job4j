c4539752b1f5010208e648d99ee6ac84d3a80746
Constantine-M <mez365@mail.ru>
2021-05-22 11:21:58 +0300

    2. Идентичные тексты [#212655]

---
+++ src/test/java/ru/job4j/collection/set/UniqueTextTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.collection.set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UniqueTextTest {
+    /**
+     * В данном тесте метод обращает внимание на запятые.
+     * Это было любопытно. К этому я шел 2 часа((.
+     */
+    @Test
+    public void whenTextsAreNotEqualsBecauseOfComma() {
+        String origin = "Harry, you are";
+        String dublicate = "are you Harry";
+        assertThat(UniqueText.isEquals(origin, dublicate), is(false));
+    }
+
+    /**
+     * А еще метод регистрозависимый. К этому я тоже долго шел.
+     */
+    @Test
+    public void whenTextsAreEquals() {
+        String origin = "Harry you are a wizard";
+        String dublicate = "a wizard are you Harry";
+        assertThat(UniqueText.isEquals(origin, dublicate), is(true));
+    }
+
+    @Test
+    public void whenTextsAreNotEquals() {
+        String origin = "My cat eats a mouse";
+        String dublicate = "A mouse is eaten by a cat";
+        assertThat(UniqueText.isEquals(origin, dublicate), is(false));
+    }
+}
