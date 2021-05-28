8e81ea78cd4dff758c1a8b631deda6a11e8a84c1
Constantine-M <mez365@mail.ru>
2021-05-28 08:02:21 +0300

    Банковский перевод

---
+++ src/main/java/ru/job4j/collection/map/bank/BankService.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection.map.bank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 3. Банковские переводы.
+ * Данный класс описывает банковский сервис.
+ */
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    
+}
