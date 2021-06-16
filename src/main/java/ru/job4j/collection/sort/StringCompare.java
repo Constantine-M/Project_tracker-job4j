651ff11e29d2ca2e491ab2e304aeb4d73a3c0e08
Constantine-M <mez365@mail.ru>
2021-06-16 12:38:36 +0300

    3. Компаратор для строк [#35008 #168176] - правки

--- src/main/java/ru/job4j/collection/sort/StringCompare.java
+++ src/main/java/ru/job4j/collection/sort/StringCompare.java
@@ -8,11 +8,13 @@ public class StringCompare implements Comparator<String> {
      * Здесь мы циклом проходим по спискам. Длину выбрал наименьшую, т.к.
      * с наибольшей выходит Out of bound Ex.
      * Вынес в отдельные переменные символы в списках с помощью charAt().
-     * Чтобы начать сравнивать, нужно, чтобы символы не были одинаковыми.
-     * С помощью Character.getNumericValue() мы преобразовали символ в число,
-     * а потом эти числа сравнили с помощью Integer.compare().
+     * Поскольку в Java для представления символов в строках используется кодировка Unicode,
+     * разрядность типа char в этом языке — 16 бит.
+     * Хотя величины типа char и не используются, как целые числа, вы можете оперировать
+     * с ними так, как если бы они были целыми.
+     * В данной задаче уместнее и экономнее использовать метод Character.compare().
      * Результать нам вернет 1 или -1. А если списки равны, то во втором return
-     * мы просто вычтем длины. Так как они одинаковые (слова), то мы получим 0.
+     * мы воспользуемся Integer.compare(). Так как они одинаковые (слова), то мы получим 0.
      * @param left первый список.
      * @param right второй список.
      * @return возвращает 0, 1 или -1.
@@ -24,9 +26,9 @@ public class StringCompare implements Comparator<String> {
             char a = left.charAt(index);
             char b = right.charAt(index);
             if (a != b) {
-                return Integer.compare(Character.getNumericValue(a), Character.getNumericValue(b));
+                return Character.compare(a, b);
             }
         }
-        return left.length() - right.length();
+        return Integer.compare(left.length(), right.length());
     }
 }
