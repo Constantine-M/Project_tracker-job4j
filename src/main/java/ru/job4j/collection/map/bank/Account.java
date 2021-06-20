42b2d98869f440ff83219ade64028b199d27ea17
Constantine-M <mez365@mail.ru>
2021-06-20 13:35:39 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/collection/map/bank/Account.java
+++ src/main/java/ru/job4j/collection/map/bank/Account.java
@@ -4,7 +4,9 @@ import java.util.Objects;
 
 /**
  * 3. Банковские переводы.
- * Данный класс описывает бансковский счет.
+ * Данный класс описывает бансковский счет пользователя.
+ * @author Constantine
+ * @version 1.0
  */
 public class Account {
     private String requisite;
@@ -15,22 +17,42 @@ public class Account {
         this.balance = balance;
     }
 
+    /**
+     * @return возвращает реквизиты пользователя.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Данный метод изменяет реквизиты пользователя.
+     * @param requisite реквизиты пользователя.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * @return возвращает баланс счета пользователя.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Данный метод изменяет баланс счета пользователя.
+     * @param balance баланс счета пользователя.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Данный метод переопределяет метод {@link Object#equals(Object)}
+     * @param o объект класса {@link #Account(String, double)}
+     * @return возвращает {@code true} если объекты равны,
+     * иначе {@code false}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -43,6 +65,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Данный метод переопределяет метод {@link Object#hashCode()}
+     * @return возвращает хэш-код объекта
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/collection/map/bank/User.java
+++ src/main/java/ru/job4j/collection/map/bank/User.java
@@ -5,9 +5,20 @@ import java.util.Objects;
 /**
  * 3. Банковские переводы.
  * Данный класс описывает пользователя банка.
+ * @author Constantine
+ * @version 1.0
  */
 public class User {
+
+    /**
+     * Данное поле описывает паспортные данные
+     * пользователя - серия/номер к примеру.
+     */
     private String passport;
+
+    /**
+     * Данное поле описывает имя пользователя - ФИО.
+     */
     private String username;
 
     public User(String passport, String username) {
@@ -15,22 +26,44 @@ public class User {
         this.username = username;
     }
 
+    /**
+     * @return возвращает паспортные данные -
+     * серия/номер например.
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Данный метод изменяет паспортные данные.
+     * @param passport паспортные данные -
+     *        серия/номер например.
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * @return возвращает имя пользователя.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Данный метод изменяет имя пользователя.
+     * @param username имя пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Данный метод переопределяет метод {@link Object#equals(Object)}
+     * @param o объект класса {@link #User(String, String)}
+     * @return возвращает {@code true} если объекты равны,
+     * иначе {@code false}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -43,6 +76,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Данный метод переопределяет метод {@link Object#hashCode()}
+     * @return возвращает хэш-код объекта
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
