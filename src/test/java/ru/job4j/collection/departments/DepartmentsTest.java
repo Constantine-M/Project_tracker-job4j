6f1137a79045fccd2ef2ab9fba193f3c8bec0613
Constantine-M <mez365@mail.ru>
2021-06-29 19:57:22 +0300

    2. Отсортировать департаменты [#34352] #2

--- src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -19,6 +19,14 @@ public class DepartmentsTest {
     }
 
     @Test
+    public void whenManyMissed() {
+        List<String> input = Arrays.asList("k1/sk1/ssk1/skk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk1/skk1");
+        List<String> rsl = Departments.fillGaps(input);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1");
