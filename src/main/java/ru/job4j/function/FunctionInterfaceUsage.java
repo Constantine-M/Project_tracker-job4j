5c6d74a7ea529edd03e9f72c5d4788e273708846
Constantine-M <mez365@mail.ru>
2021-07-07 20:21:49 +0300

    1.2. Функциональный интерфейс [#247497] - #2

--- src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
+++ src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
@@ -3,9 +3,22 @@ package ru.job4j.function;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class FunctionInterfaceUsage {
+    /**
+     * В данном методе пробежимся по
+     * основным функциональным интерфейсам.
+     * 1.Supplier (поставщик) используется для создания
+     * какого-либо объекта и при этом ему
+     * не требуется входной параметр.
+     * 2.Consumer (потребитель) используется в том случае,
+     * если нам нужно применить какое-то
+     * действие или операцию к параметру
+     * 3.
+     */
     public static void main(String[] args) {
         Supplier<String> sup = () -> "New String For Interface";
         System.out.println(sup.get());
@@ -15,6 +28,20 @@ public class FunctionInterfaceUsage {
         Set<String> strings = sup1.get();
         for (String s : strings) {
             System.out.println(s);
+        }
+
+        Consumer<String> consumer = (s) -> System.out.println(s);
+        consumer.accept(sup.get());
+        BiConsumer<String, String> biConsumer = (s, s1) -> System.out.println(s + s1);
+        biConsumer.accept(sup.get(), " and second String");
+
+        List<String> list1 = List.of("one", "two", "three", "one", "two", "three");
+        Supplier<Set<String>> sup2 = () -> new HashSet<>(list1);
+        BiConsumer<Integer, String> secondConsumer = (s, s1) -> System.out.println(s + s1);
+        Set<String> secondStrings = sup2.get();
+        int i = 1;
+        for (String s : secondStrings) {
+            secondConsumer.accept(i++, " is " + s);
         }
     }
 }
