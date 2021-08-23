584d5b5c9c4c3819699801fe3ce2e57cc2ea92bd
Constantine-M <mez365@mail.ru>
2021-08-23 23:25:09 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228] #2

--- src/test/java/ru/job4j/stream/flatmap/UsageFlatMapTest.java
+++ src/test/java/ru/job4j/stream/flatmap/UsageFlatMapTest.java
@@ -25,7 +25,7 @@ public class UsageFlatMapTest {
         };
         List<Integer> result = toList.convert(matrix);
         List<Integer> expected = List.of(
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15, 16);
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
         assertThat(result, is(expected));
     }
 
