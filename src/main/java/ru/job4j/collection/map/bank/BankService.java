28a8e8383770b03244fe7799c39af22a94492308
Constantine-M <mez365@mail.ru>
2021-09-05 20:52:50 +0300

    4. Optional в Stream API. [#279130 #197326] - fixes #5

--- src/main/java/ru/job4j/collection/map/bank/BankService.java
+++ src/main/java/ru/job4j/collection/map/bank/BankService.java
@@ -47,10 +47,11 @@ public class BankService {
      */
     public void addAccount(String passport, Account account) {
         Optional<User> user = findByPassport(passport);
-        List<Account> listAccount = users.get(user.get());
-        if (user.isPresent()
-            && !listAccount.contains(account)) {
-            listAccount.add(account);
+        if (user.isPresent()) {
+            List<Account> listAccount = users.get(user.get());
+            if (!listAccount.contains(account)) {
+                listAccount.add(account);
+            }
         }
     }
 
