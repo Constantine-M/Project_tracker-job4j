0e2892994d0befd3ae3c3b90e12396be6b557d4a
ConstantineM <mez365@mail.ru>
2023-12-13 22:18:23 +0300

    2. @Setter, @Getter и другие [#504889]

---
+++ src/main/java/ru/job4j/course/lombok/Category.java
@@ -0,0 +1,39 @@
+package ru.job4j.course.lombok;
+
+import lombok.*;
+
+/**
+ * Данный класс демонстрирует базовые
+ * аннотации при использовании библиотеки
+ * Lombok.
+ *
+ * Аннотация {@link EqualsAndHashCode}
+ * – создаёт методы equals() и @hashCode().
+ * Эта аннотация имеет параметры, в которых
+ * можно указать какие поля использовать
+ * для вычисления hashCode, equals.
+ *
+ * По умолчанию используются все не статические
+ * поля. Чтобы явно указать, какие поля
+ * использовать в equals и hashCode нужно
+ * включить параметр onlyExplicitlyIncuded
+ * и отметить нужные поля аннотацией
+ * {@link EqualsAndHashCode.Include}.
+ *
+ * Аннотация {@link RequiredArgsConstructor}
+ * создает конструктор по полям отмеченным
+ * {@link NonNull}. В нашем случае это поле id.
+ */
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Category {
+
+    @NonNull
+    @EqualsAndHashCode.Include
+    @Getter
+    private int id;
+
+    @Getter
+    @Setter
+    private String name;
+}
